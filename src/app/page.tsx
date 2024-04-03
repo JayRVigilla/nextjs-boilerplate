ea3b3ee73104945928d00c57834655bc7ea2ae09
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-03 13:52:28 -0700

    sanity save

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,8 @@ import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.home}></div>
+    <div className={styles.home}>
+      <h1>Boiler Plate Empty</h1>
+    </div>
   );
 }
