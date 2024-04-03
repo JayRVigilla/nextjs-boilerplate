4978bd3c30422c19975b05290cfdf255773e3abd
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-03 08:03:40 -0700

    IconLink story initiated

---
+++ src/components/IconLink/IconLink.tsx
@@ -0,0 +1,34 @@
+/** IconLink documentation
+ */
+
+import {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
+import clsx from "clsx";
+
+// import styles from "./styles.module.css";
+import "./styles.css";
+import { IconDefinition } from "@fortawesome/fontawesome-common-types";
+
+export interface IconLinkProps {
+  "data-test-id"?: string;
+  href: string;
+  label: string;
+  icon: IconDefinition;
+}
+
+export const IconLink = ({href, label, icon, "data-test-id": dataTestId}: IconLinkProps) => {
+  return (
+    <a
+      href={href}
+      rel="noreferrer"
+      target="_blank"
+      key={`link-${label}`}
+      aria-label={label}
+      data-test-id={dataTestId}
+      // className={clsx(styles.root)}
+      className="root"
+    >
+      <span className='label'>{label}</span>
+      <FontAwesomeIcon icon={icon} />
+    </a>
+  );
+};
--- src/components/IconLink/index.tsx
+++ src/components/IconLink/index.tsx
@@ -1,32 +1,3 @@
-/** IconLink documentation
- */
+import { IconLink } from "./IconLink";
 
-import {FontAwesomeIcon}  from "@fortawesome/react-fontawesome";
-import clsx from "clsx";
-
-import styles from "./styles.module.css";
-import { IconDefinition } from "@fortawesome/fontawesome-common-types";
-
-export interface IconLinkProps {
-  "data-test-id"?: string;
-  href: string;
-  label: string;
-  icon: IconDefinition;
-}
-
-export const IconLink = ({href, label, icon, "data-test-id": dataTestId}: IconLinkProps) => {
-  return (
-    <a
-      href={href}
-      rel="noreferrer"
-      target="_blank"
-      key={`link-${label}`}
-      aria-label={label}
-      data-test-id={dataTestId}
-      className={clsx(styles.root)}
-    >
-      <span className='label'>{label}</span>
-      <FontAwesomeIcon icon={icon} />
-    </a>
-  );
-};
+export {IconLink}
