cc94109fc0271b6294bdae03f1eca1d42d04e4e3
Jay Vigilla <jayrvigilla@gmail.com>
2024-04-09 11:58:11 -0700

    wordpress utils update

--- src/utils/wordpress.ts
+++ src/utils/wordpress.ts
@@ -1,7 +1,8 @@
 import { walkDOMChildren } from "./dom";
 
+export const WORDPRESS_PUBLIC_API_BASE_URL = `https://public-api.wordpress.com/wp/v2/sites`
 // use .env to generate WordPress API base url
-export const WORDPRESS_PUBLIC_API_BASE_URL = `https://public-api.wordpress.com/wp/v2/sites/${process.env.REACT_APP_WORDPRESS_URL}/sites/`
+export const WORDPRESS_SITE_API = `${WORDPRESS_PUBLIC_API_BASE_URL}/${process.env.REACT_APP_WORDPRESS_URL}`
 
 
 // Posts and Pages have content.rendered as strings of the HTML
