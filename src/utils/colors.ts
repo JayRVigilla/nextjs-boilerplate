8b6cd4baf01c58514f5aedc64b49c398d84f0cfa
Jay Vigilla <jayrvigilla@gmail.com>
2024-06-25 17:37:27 -0700

    comments

--- src/utils/colors.ts
+++ src/utils/colors.ts
@@ -1,3 +1,11 @@
+/**
+ * These are color variables to use in .ts/.tsx files. The
+ * naming conventions matches with the css color variables
+ * (camelCase for .ts/.tsx, kebab-case for css). When adding or
+ * changing a color variable, make sure to update in this file
+ * and src/app/globals.css .
+ */
+
 const style = getComputedStyle(document.body);
 
 export const COLORS = {
